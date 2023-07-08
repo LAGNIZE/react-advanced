153b453cb463ddde46619284c5926af654ba582c
Aleksei Demichev <a.demichev@potok.team>
2023-07-08 13:56:08 +0300

    update 07/08/2023

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,21 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'helpers/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { NavBar } from 'widgets/NavBar';
+import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <NavBar />
-      <AppRouter />
+      <Suspense fallback="">
+        <NavBar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'helpers/classNames/classNames';
 import { Link } from 'react-router-dom';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './NavBar.module.scss';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 
 interface NavBarProps {
   className?: string;
@@ -11,7 +10,6 @@ interface NavBarProps {
 export const NavBar = ({ className }: NavBarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
